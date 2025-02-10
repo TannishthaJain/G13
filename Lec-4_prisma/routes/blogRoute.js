19911f8e1dbf7d524d54678879616f1dde66de49
Nitesh <nitesh.e21@nsut.ac.in>
2025-02-10 23:25:28 +0530

    JWT addition

---
+++ Lec-4_prisma/routes/blogRoute.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router= express.Router();
+const { PrismaClient } = require('@prisma/client');
+const isLoggedIn = require("../middleware/verifylogin");
+const prisma = new PrismaClient();
+router.post("/",isLoggedIn, async(req,res)=>{
+    const {title,description} = req.body;
+    console.log(req.user)
+    const newBlog = await prisma.blog.create({
+        data:{
+            Title:title,
+            description:description,
+            authorId:req.user.id
+        }
+    })
+    res.json({message:"blog added successfully",
+        data:newBlog
+    })
+
+})
+router.get("/:id",async(req,res)=>{
+    const {id}=req.params;
+    const blog=prisma.blog.findUnique({
+        where:{
+            id:parseInt(id)
+        }
+    })
+    res.json({data:blog})
+})
+
+
+
+module.exports=router
